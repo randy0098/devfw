fea47df6d7f043206444b170567d853da1795d5c
randy.xia98@gmail.com <randy.xia98@gmail.com@23136ec8-3fb6-5578-5818-a9efab4f5e9f>
2011-12-23 06:37:58 +0000

    添加DAO功能

---
+++ trunk/src/dao/DAOController.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright Notice ====================================================
+ * This file contains proprietary information of SNS.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 2010   All rights reserved. ======================
+ */
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DAOController
+{
+	private Connection conn = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+
+	public DAOController(){
+		conn = DBConnection.getConnection();
+	}
+	
+	public boolean insert(String sql){
+		boolean result = false;
+		try
+        {
+	        ps = conn.prepareStatement(sql);
+	        result = ps.execute();
+        }
+        catch (SQLException e)
+        {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        }
+        return result;
+	}
+	
+	/**
+	 * 
+	 * 修改记录
+	 *
+	 */
+	public int update(String sql){
+		int result = 0;
+		try
+        {
+	        ps = conn.prepareStatement(sql);
+	        result = ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        }
+		return result;
+	}
+	
+	/**
+	 * 
+	 * 查询记录
+	 *
+	 */
+	public ResultSet select(String sql){
+		try
+        {
+	        ps = conn.prepareStatement(sql);
+	        rs = ps.executeQuery();
+        }
+        catch (SQLException e)
+        {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        }
+		return rs;
+	}
+	
+	/**
+	 * 
+	 * 删除记录
+	 *
+	 */
+	public boolean delete(String sql){
+		boolean result = false;
+		try
+        {
+	        ps = conn.prepareStatement(sql);
+	        result = ps.execute();
+        }
+        catch (SQLException e)
+        {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        }
+        return result;
+	}
+	
+	public void close(){
+		if(rs!=null){
+			try
+            {
+	            rs.close();
+            }
+            catch (SQLException e)
+            {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+            }
+		}
+		if(ps!=null){
+			try
+            {
+	            rs.close();
+            }
+            catch (SQLException e)
+            {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+            }
+		}
+		if(conn!=null){
+			try
+            {
+	            rs.close();
+            }
+            catch (SQLException e)
+            {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+            }
+		}
+	}
+}
